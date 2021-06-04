ba3138089c844ed7b28b94be123281b046c1e154
ruLegen <forgame98@yandex.ru>
2021-06-04 22:53:09 +0300

    First init

---
+++ Source/FolderDefense/FolderDefenseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FolderDefenseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FOLDERDEFENSE_API AFolderDefenseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
